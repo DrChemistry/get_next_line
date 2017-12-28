6aac807fe11eb6b904667d16f2d22828a8c5883f
DrChemistry <alexdirosa@laposte.net>
2017-12-28 13:02:14 +0100

    d

--- get_next_line.c
+++ get_next_line.c
@@ -8,12 +8,25 @@ int get_next_line(const int fd, char **line)
   static char *save;
   char        *str;
   char        *buffer;
+  int         x;
+  int         i;
 
   if (!(buffer = malloc(sizeof(char) * BUFF_SIZE + 1)))
     return (-1);
+    i = 0;
   while (read(fd, buffer, BUFF_SIZE))
   {
-    printf("%s\n", buffer);
+    x = 0;
+    if (!str && !(str = malloc(sizeof(char) * BUFF_SIZE + 1))
+    return (-1);
+    while (buffer[x] && buffer[x] != '\n')
+        str[i++] = buffer[x++];
+    if (buffer[x] == '\n')
+    {
+      str[i] = '\0';
+      *line = str;
+      return (0);
+    }
   }
   return (-1);
 }
