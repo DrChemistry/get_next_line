5b0ed006c15cc1101ef418dcf8843b4e0670d0ab
DrChemistry <alexdirosa@laposte.net>
2017-12-28 15:46:15 +0100

    d

--- main.c
+++ main.c
@@ -3,8 +3,8 @@ int get_next_line(const int, char **);
 
 int main(int ac, char **av)
 {
-  char *line;
-  get_next_line(0, &line);
+  char **line;
+  get_next_line(0, line);
   printf("%s\n", line[0]);
   return (0);
 }
