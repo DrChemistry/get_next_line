24f255063e2ff76582febcb700e2ea761473bbc9
DrChemistry <alexdirosa@laposte.net>
2017-12-28 15:48:56 +0100

    d

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 int get_next_line(const int, char **);
 
 int main(int ac, char **av)
 {
   char **line;
+  line = malloc(sizeof(char *) * 1);
   get_next_line(0, line);
   printf("%s\n", line[0]);
   return (0);
